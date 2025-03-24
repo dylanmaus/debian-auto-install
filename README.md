11ed30d4af9e299d7a8bc6af424c3d630aa99a30
dylanmaus <dylan.p.maus@gmail.com>
2025-03-24 01:00:04 -0400

    repack iso

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# debian-auto-install
+# Debian auto install
 Debian auto install with full disk encryption
+
+## Repack bootable ISO
+https://wiki.debian.org/RepackBootableISO
---
+++ repack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# https://wiki.debian.org/RepackBootableISO
+
+# The example names get mapped to their roles here
+orig_iso="$HOME"/debian-12.10.0-amd64-netinst.iso
+new_files=/tmp/debisocustom
+new_iso="$HOME"/preseed-debian-12.10.0-amd64-netinst.iso
+mbr_template=isohdpfx.bin
+
+# Extract MBR template file to disk
+dd if="$orig_iso" bs=1 count=432 of="$mbr_template"
+
+# Create the new ISO image
+xorriso -as mkisofs \
+   -r -V 'Debian 12.10.0 amd64 n' \
+   -o "$new_iso" \
+   -J -J -joliet-long -cache-inodes \
+   -isohybrid-mbr "$mbr_template" \
+   -b isolinux/isolinux.bin \
+   -c isolinux/boot.cat \
+   -boot-load-size 4 -boot-info-table -no-emul-boot \
+   -eltorito-alt-boot \
+   -e boot/grub/efi.img \
+   -no-emul-boot -isohybrid-gpt-basdat -isohybrid-apm-hfsplus \
+   "$new_files"
