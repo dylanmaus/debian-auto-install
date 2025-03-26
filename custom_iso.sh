324fb21954108e0126c63fb3523be9c9eeadd696
dylanmaus <dylan.p.maus@gmail.com>
2025-03-25 23:00:32 -0400

    cleanup

--- custom_iso.sh
+++ custom_iso.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# https://wiki.debian.org/RepackBootableISO
-
 working_dir=`pwd`
-echo $working_dir
 orig_iso=debian-12.10.0-amd64-netinst.iso
 orig_iso_mnt=/tmp/debian
 custom_files=/tmp/custom_debian
@@ -15,7 +12,6 @@ preseed_url=http://192.168.1.7:8000/preseed/preseed.cfg
 if [ ! -f $orig_iso ]; then
    wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/$orig_iso
 fi
-# sudo chown -R $USER $custom_files
 
 # mount and unpack ISO
 mkdir -p $orig_iso_mnt
@@ -32,22 +28,22 @@ echo "set timeout=2" >> $custom_files/boot/grub/grub.cfg
 
 cd $working_dir
 
-# Extract MBR template file to disk
-dd if="$orig_iso" bs=1 count=432 of="$mbr_template"
+# extract MBR template file to disk
+dd if=$orig_iso bs=1 count=432 of=$mbr_template
 
-# Create the new ISO image
+# create new ISO image
 xorriso -as mkisofs \
    -r -V 'Debian 12.10.0 amd64 n' \
    -o "$new_iso" \
    -J -J -joliet-long -cache-inodes \
-   -isohybrid-mbr "$mbr_template" \
+   -isohybrid-mbr $mbr_template \
    -b isolinux/isolinux.bin \
    -c isolinux/boot.cat \
    -boot-load-size 4 -boot-info-table -no-emul-boot \
    -eltorito-alt-boot \
    -e boot/grub/efi.img \
    -no-emul-boot -isohybrid-gpt-basdat -isohybrid-apm-hfsplus \
-   "$custom_files"
+   $custom_files
 
 # unmount and delete temporary files
 sudo umount $orig_iso_mnt
