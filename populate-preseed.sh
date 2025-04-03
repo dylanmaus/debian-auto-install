150e8ee1d518659dc5041ef13726b9def82f4844
dylanmaus <dylan.p.maus@gmail.com>
2025-04-02 23:01:59 -0400

    create ssh key pair for dropbear

--- populate-preseed.sh
+++ populate-preseed.sh
@@ -6,17 +6,17 @@ root_password=$3
 crypto_password=$4
 
 # populate user info
-sed -i s"|defaultusername|$username|g" "$name"/custom-preseed.cfg
-sed -i s"|defaultuserpassword|$user_password|g" "$name"/custom-preseed.cfg
+sed -i s"|defaultusername|$username|g" "$name"/preseed.cfg
+sed -i s"|defaultuserpassword|$user_password|g" "$name"/preseed.cfg
 
 # populate root password
-sed -i s"|defaultrootpassword|$root_password|g" ./"$name"/custom-preseed.cfg
+sed -i s"|defaultrootpassword|$root_password|g" ./"$name"/preseed.cfg
 
 # populate disk
-sed -i s"|defaultdisk|$disk|g" "$name"/custom-preseed.cfg
+sed -i s"|defaultdisk|$disk|g" "$name"/preseed.cfg
 
 # populate crypto password
-sed -i s"|defaultcryptopassword|$crypto_password|g" "$name"/custom-preseed.cfg
+sed -i s"|defaultcryptopassword|$crypto_password|g" "$name"/preseed.cfg
 
 # populate post preseed url
-sed -i s"|defaulturl|$preseed_url|g" "$name"/custom-preseed.cfg
+sed -i s"|defaulturl|$preseed_url|g" "$name"/preseed.cfg
--- run.sh
+++ run.sh
@@ -6,16 +6,18 @@
 # get user password from input
 read -p "Enter user password: " user_password
 
-echo "$user_password"
-
-# create dir for files
-mkdir -p "$name"
-
 # create files containing generated keys
 crypto_password=`openssl rand -base64 32 | sed -r 's/[/=]/_/g'`
 root_password=`openssl rand -base64 32 | sed -r 's/[/=]/_/g'`
 echo -n "$crypto_password" > ./"$name"/crypto.key
 echo -n "$root_password" > ./"$name"/root.key
+
+# create ssh key pair for dropbear
+ssh-keygen -q -t ed25519 -N "" -f ~/.ssh/"$name" <<< y
+
+# create config for dropbear
+echo username="$username" > ./"$name"/dropbear.conf
+echo ssh_public_key=`cat ~/.ssh/machine0.pub` >> ./"$name"/dropbear.conf
 
 # download Debian ISO if it doesn't exist already
 if [ ! -f "$orig_iso" ]; then
@@ -23,16 +25,16 @@ if [ ! -f "$orig_iso" ]; then
 fi
 
 # mount and unpack ISO to modifiable location
-./mount_unpack_iso.sh "$orig_iso"
+./mount-unpack-iso.sh "$orig_iso"
 
 # modify grub.cfg to auto install with preseed
-./modify_grub.sh "$preseed_url" "$network_interface"
+./modify-grub.sh "$preseed_url" "$network_interface"
 
 # repack bootable ISO
-./repack_bootable_iso.sh "$orig_iso" "$name"/"$new_iso"
+./repack-bootable-iso.sh "$orig_iso" "$name"/"$new_iso"
 
 # populate preseed.cfg
-cp default-preseed.cfg "$name"/custom-preseed.cfg
+cp default-preseed.cfg "$name"/preseed.cfg
 ./populate-preseed.sh $1 "$user_password" "$root_password" "$crypto_password"
 
 # unmount and delete temporary files
