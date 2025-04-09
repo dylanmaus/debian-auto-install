06c3dbde227ddd46c4dd44d110bc3a7de239e64a
dylanmaus <dylan.p.maus@gmail.com>
2025-04-08 22:03:18 -0400

    unlock

--- tmphost/post-preseed.sh
+++ tmphost/post-preseed.sh
@@ -12,6 +12,9 @@ echo 'DROPBEAR_OPTIONS="-I 180 -j -k -p 2222 -s -c cryptroot-unlock"' >> /etc/dr
 # add ssh public key to authorized keys
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3nhsGyRXG06d7umd0V0BUve78TBgd/RahhWSLeZEAh user@machine1" > /etc/dropbear/initramfs/authorized_keys
 
+chmod 600 /etc/dropbear/initramfs/authorized_keys
+chown root:root /etc/dropbear/initramfs/authorized_keys
+
 echo 'IP="dhcp"' >> /etc/initramfs-tools/initramfs.conf
 update-initramfs -u -k all
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet ip=dhcp"/g' /etc/default/grub
